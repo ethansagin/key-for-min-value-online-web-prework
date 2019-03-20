e7ed4942d89b1e88f5975a1ff33c944c5404bb9a
Ethan <ethansagin@gmail.com>
2019-03-20 18:50:45 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,24 +4,8 @@
 require 'pry'
 
 def key_for_min_value(name_hash)
- if name_hash == {}
-   return nil
- else
-  value_arr = name_hash.map do |key, value|
-    value
-  end
-  while value_arr.length > 1
-    if value_arr[0] > value_arr[1]
-      value_arr.shift
-    else
-      value_arr.delete_at(1)
-      value_arr
-    end
-  end
-  name_hash.select do |key, value| 
-    if value == value_arr.join
-      key
-    end
-  end
- end
-end
+  if name_hash == {}
+    return nil
+  else
+    min_val = nil
+    min_key = nil
