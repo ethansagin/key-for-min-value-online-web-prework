1678332d43e17c7f0ca590b32da7f0bad8e32465
Ethan <ethansagin@gmail.com>
2019-03-20 18:07:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,9 @@ def key_for_min_value(name_hash)
   value_arr = name_hash.map do |key, value| {
     value
   }
-  if key == value
-    key
+  if value_arr[0] > value_arr[1]
+    value_arr.shift
+  else
+    value_arr.
   end
 end
