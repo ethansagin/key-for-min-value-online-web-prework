8ea8a5e62d7fa6166864d82141dd5fe8b21243ba
Ethan <ethansagin@gmail.com>
2019-03-20 18:19:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -13,8 +13,7 @@ def key_for_min_value(name_hash)
     end
   end
   name_hash.each do |key, value| 
-    if value == value_arr.join()
-      return key
-    end
+    answer = key if value == value_arr.join()
   end
+  answer
 end
